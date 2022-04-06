25ef595267dfb54f91cbd92bbcd08d4c186bea3e
Liam Pugh <liam.pugh.009@gmail.com>
2022-04-06 19:23:26 +0100

    Confuigured for linux too

--- data/data.js
+++ data/data.js
@@ -28,12 +28,12 @@ const pages = [
 const categories = [
     {
         title: 'Nulla fermentum tellus',
-        content: 'Curae quis ultrices duis pellentesque tristique tellus risus vivamus et nascetur penatibus. Montes consectetur fames et aliquet malesuada adipiscing dictumst tincidunt dignissim nullam. Porta risus inceptos etiam ultricies ipsum facilisi ac. Letius vehicula mus odio inceptos class. Vestibulum euismod cursus laoreet vivamus libero rutrum. Orci est consequat egestas facilisi cubilia taciti amet odio nibh sed. Sollicitudin volutpat maecenas pharetra lorem congue ut nullam bibendum facilisi imperdiet. Litora fermentum integer pharetra justo purus eleifend quam vehicula lectus.',
+        content: 'Curae quis ultrices risus vivamus et nascetur penatibus. Montes consectetur fames et aliquet malesuada adipiscing dictumst tincidunt.',
         Product: {
             create: [
                 {
                     title: 'Aenean sagittis elit lorem finibus',
-                    content: 'Dictumst ut eros finibus tincidunt sapien orci magnis accumsan nascetur. Libero auctor a curabitur etiam si dui. Interdum libero lacinia mi lacus nulla. Integer dolor nostra habitant id quam. Torquent parturient porta eleifend tortor vitae mauris faucibus ut. Pharetra curabitur feugiat turpis euismod nulla ullamcorper blandit non magnis enim diam. Vehicula semper nec risus consequat cursus elit fames finibus quam porttitor praesent. Tempor erat suscipit nunc faucibus condimentum.',
+                    content: 'Dictumst ut magnis accumsan nascetur. Libero auctor a curabitur etiam si dui. Interdum libero lacinia mi lacus nulla. Integer dolor nostra habitant id quam. Torquent parturient porta eleifend tortor vitae mauris faucibus ut. Pharetra curabitur feugiat turpis euismod nulla ullamcorper blandit non magnis enim diam.',
                     Image: {
                         create: [
                             {
